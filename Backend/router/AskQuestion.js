8fec8be4bfaafa9fa655007ebaad4049de02f840
Soham Patil <145631825+Soham271@users.noreply.github.com>
2025-07-12 16:00:03 +0530

    Delete Backend directory

--- Backend/router/AskQuestion.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { askQuestion, getquestion } from "../controllers/AskQuestion.js";
-
-const router = express.Router();
-
-router.post("/ask", askQuestion); // POST /api/v1/question/ask
-router.get("/getques", getquestion);
-
-export default router;
