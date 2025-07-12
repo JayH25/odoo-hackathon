feef6369a364d7c00164c9b4733b2abc5dd7234c
Soham Patil <145631825+Soham271@users.noreply.github.com>
2025-07-12 16:00:24 +0530

    Delete Frontend/vite-project directory

--- Frontend/vite-project/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white text-center py-4">
-      © 2025 Odoo's Project. All rights reserved.
-    </footer>
-  );
-};
-
-export default Footer;
